af2aa2b71604310d4353077e343426ad1b288039
include <theonetwothe@gmail.com>
2022-09-12 21:29:59 +0500

    This is first commit. Created README and initial files.

---
+++ shell_2_0.c
@@ -0,0 +1,17 @@
+#include "modules/input_formatting.h"
+#include "modules/process_control.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <string.h>
+#include <termios.h>
+#include <stdint.h>
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
